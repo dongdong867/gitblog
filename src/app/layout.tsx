1f27eee7c090a83d1137e5fbdebd2a8096dec5bd
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-02-23 15:26:33 +0800

    feat(layout): import colors css file to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import "./colors.css";
 import "./globals.css";
 import ThemeProvider from "@/components/ThemeProvider";
 
