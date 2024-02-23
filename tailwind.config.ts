0db3d6620e136d8f60b92d8eb905a7d9cad46e81
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-02-23 15:24:57 +0800

    feat(colors): finish color settings with tailwind name

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,30 @@ const config = {
       },
     },
     extend: {
+      colors: {
+        foreground: "rgb(var(--foreground))",
+        background: "rgb(var(--background))",
+        white: "rgb(var--white)",
+        black: "rgb(var(--black))",
+        red: "rgb(var(--red))",
+        orange: "rgb(var(--orange))",
+        yellow: "rgb(var(--yellow))",
+        green: "rgb(var(--green))",
+        mint: "rgb(var(--mint))",
+        teal: "rgb(var(--teal))",
+        cyan: "rgb(var(--cyan))",
+        blue: "rgb(var(--blue))",
+        indigo: "rgb(var(--indigo))",
+        purple: "rgb(var(--purple))",
+        pink: "rgb(var(--pink))",
+        brown: "rgb(var(--brown))",
+        gray: "rgb(var(--gray))",
+        gray2: "rgb(var(--gray2))",
+        gray3: "rgb(var(--gray3))",
+        gray4: "rgb(var(--gray4))",
+        gray5: "rgb(var(--gray5))",
+        gray6: "rgb(var(--gray6))"
+      },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
