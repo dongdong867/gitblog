8a9718b3b17a4f57ead3b8587eb4eea8f7609ec0
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-02-23 12:21:00 +0800

    feat(ThemeProvider): create component

---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Button } from "./ui/button";
+import { MdSunny, MdDarkMode } from "react-icons/md";
+import { cn } from "@/lib/utils";
+
+const ThemeToggle = () => {
+  const { setTheme } = useTheme();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant={"outline"} size={"icon"}>
+          <MdSunny className={cn("size-full p-2 dark:hidden")} />
+          <MdDarkMode className={cn("size-full p-2 hidden dark:block dark:scale-100")} />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default ThemeToggle;
