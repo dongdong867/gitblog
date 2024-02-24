232eb4924b1733f45a08fe6250da98c809001363
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-02-24 08:28:21 +0800

    refactor(ThemeToggle): set dropdown item into reusable component

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useTheme } from "next-themes";
+// components
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -8,30 +8,48 @@ import {
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
 import { Button } from "./ui/button";
+// icons
 import { MdSunny, MdDarkMode } from "react-icons/md";
+// utils
 import { cn } from "@/lib/utils";
+import { useTheme } from "next-themes";
 
-const ThemeToggle = () => {
+const ThemeDropdownItem = ({
+  themeOption,
+}: {
+  themeOption: "Light" | "Dark" | "System";
+}) => {
   const { setTheme } = useTheme();
 
   return (
+    <DropdownMenuItem
+      onClick={() => setTheme(themeOption.toLowerCase())}
+      className="text-base"
+    >
+      {themeOption}
+    </DropdownMenuItem>
+  );
+};
+
+const ThemeToggle = () => {
+  return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant={"outline"} size={"icon"}>
-          <MdSunny className={cn("size-full p-2 dark:hidden")} />
-          <MdDarkMode className={cn("size-full p-2 hidden dark:block dark:scale-100")} />
+        <Button
+          variant={"ghost"}
+          size={"icon"}
+          className="size-12 rounded-full"
+        >
+          <MdSunny className={cn("size-full p-2 text-primary dark:hidden")} />
+          <MdDarkMode
+            className={cn("size-full p-2 hidden dark:block dark:scale-100")}
+          />
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent>
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
+        <ThemeDropdownItem themeOption="Light" />
+        <ThemeDropdownItem themeOption="Dark" />
+        <ThemeDropdownItem themeOption="System" />
       </DropdownMenuContent>
     </DropdownMenu>
   );
