318a4044d9234884f34617224fe4078931c6f83d
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-02-24 18:03:00 +0800

    feat(auth): update token scope and save in cookies

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,27 @@
 import NextAuth from "next-auth"
+import { cookies } from "next/headers"
 import GitHub from "next-auth/providers/github"
 
 export const {
   handlers: { GET, POST },
   auth,
 } = NextAuth({
-  providers: [GitHub],
+  providers: [GitHub({
+    clientId: process.env.AUTH_GITHUB_ID,
+    clientSecret: process.env.AUTH_GITHUB_SECRET,
+    authorization: {
+      url: "https://github.com/login/oauth/authorize",
+      params: { scope: "read:user user:email repo" }
+    }
+  })],
+  callbacks: {
+    async signIn(params) {
+      const cookieStore = cookies()
+      if (params.account?.access_token) {
+        cookieStore.set("api_token", params.account?.access_token!)
+        return true
+      }
+      return false
+    }
+  }
 })
