ebd39a4807151ae9c0c4acf40d5fd848d7d52f29
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-02-26 03:08:02 +0800

    feat(types): create issue and associate types

---
+++ src/types/issue.d.ts
@@ -0,0 +1,14 @@
+interface Issue {
+  id: number
+  title: string
+  body: string
+  created_at: string
+  state: "open" | "close"
+  comments: number
+  user: User
+  labels: {
+    name: string
+    [key: string]: any
+  } []
+  [key: string]: any
+}
