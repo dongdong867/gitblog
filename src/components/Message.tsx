714383dcb978a224c94d3668232b551148b8676d
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-02-27 14:46:39 +0800

    feat(Message): create reusable component

---
+++ src/components/Message.tsx
@@ -0,0 +1,24 @@
+// components
+import UserLabel from "./UserLabel";
+// utils
+import { cn } from "@/lib/utils";
+import { marked } from "@/lib/marked";
+
+const Message = ({ message }: { message: Message }) => {
+  return (
+    <div className={cn("py-4 space-y-4")}>
+      <div className={cn("flex justify-between place-items-center")}>
+        <UserLabel size={40} user={message.user} />
+        <div className={cn("pr-4 text-gray text-sm")}>
+          {new Date(message.created_at).toLocaleString()}
+        </div>
+      </div>
+      <div
+        dangerouslySetInnerHTML={{ __html: marked.parse(message.body) }}
+        className={cn("px-4 text-sm")}
+      />
+    </div>
+  );
+};
+
+export default Message;
