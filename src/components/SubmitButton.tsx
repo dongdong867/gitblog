57a9a32a2637fe3aee0fcd70ba26982326d91a0d
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-03-01 01:28:39 +0800

    feat(SubmitButton): create button for form submission

---
+++ src/components/SubmitButton.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useFormStatus } from "react-dom";
+import { Button } from "./ui/button";
+import { cn } from "@/lib/utils";
+
+const SubmitButton = ({ label }: { label: string }) => {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button
+      type="submit"
+      disabled={pending}
+      className={cn("bg-primary dark:bg-blue dark:text-foreground dark:hover:bg-[rgb(0,122,255)]/80")}
+    >
+      <div>{label}</div>
+    </Button>
+  );
+};
+
+export default SubmitButton;
