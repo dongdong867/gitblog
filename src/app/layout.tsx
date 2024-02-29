16b88d632179a74e25f621523730f5498caf8992
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-02-29 15:44:31 +0800

    feat(layout): add toaster to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,10 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./colors.css";
 import "./globals.css";
-import "./hljs.css"
+import "./hljs.css";
 import ThemeProvider from "@/components/ThemeProvider";
 import Navbar from "@/components/navbar/Navbar";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -29,6 +30,7 @@ export default function RootLayout({
         >
           <Navbar />
           <main>{children}</main>
+          <Toaster />
         </ThemeProvider>
       </body>
     </html>
