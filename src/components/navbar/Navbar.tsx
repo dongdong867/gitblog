cf7daccb7616d68a8cbac9d7744b6d54fcc7d899
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-02-29 14:07:30 +0800

    feat(CreatePostLink): create component and add to navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import Avatar from "./Avatar";
 import Image from "next/image";
 import ThemeToggle from "../ThemeToggle";
+import CreatePostLink from "./CreatePostLink";
 
 const Navbar = async () => {
   return (
@@ -13,12 +14,27 @@ const Navbar = async () => {
         className={cn("w-11/12 flex justify-between place-items-center m-auto")}
       >
         <Link href={"/"}>
-          <Image src={"/icon.png"} alt="" width={40} height={40} className={cn("md:hidden")} />
-          <Image src={"/logo.png"} alt="" width={120} height={40} className={cn("max-md:hidden")} />
+          <Image
+            src={"/icon.png"}
+            alt=""
+            width={40}
+            height={40}
+            className={cn("md:hidden")}
+          />
+          <Image
+            src={"/logo.png"}
+            alt=""
+            width={120}
+            height={40}
+            className={cn("max-md:hidden")}
+          />
         </Link>
 
-        <div className={cn("flex place-items-center space-x-4")}>
-          <ThemeToggle />
+        <div className={cn("flex place-items-center space-x-2")}>
+          <div className={cn("size-full flex place-items-center")}>
+            <CreatePostLink />
+            <ThemeToggle />
+          </div>
           <Avatar />
         </div>
       </div>
