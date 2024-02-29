de18da8ac79fc034fed68fbc0931991a33c205e5
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-03-01 01:01:04 +0800

    feat(CreatePage): add redirect when session not found

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,7 +1,14 @@
 import { cn } from "@/lib/utils";
 import CreatePostForm from "./form";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
-const CreatePostPage = () => {
+const CreatePostPage = async () => {
+  const session = await auth();
+  if (!session) {
+    redirect("/");
+  }
+  
   return (
     <div className={cn("container max-w-[1000px] py-4 space-y-4")}>
       <div className={cn("text-3xl font-semibold tracking-tight")}>
