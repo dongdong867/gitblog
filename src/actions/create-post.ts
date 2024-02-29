ac6fb2a7928b1ca79628b20d41e519f5efbeaa5f
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-02-29 15:49:54 +0800

    feat(create-post): update form action to handle error

--- src/actions/create-post.ts
+++ src/actions/create-post.ts
@@ -12,22 +12,41 @@ type CreatePostType = {
   labels: string[] | undefined
 }
 
-export const createPost = async (formData: FormData) => {
-  const session = (await auth()) as SessionWithToken
+export const createPost = async (
+  prevState: { error: string },
+  formData: FormData
+) => {
+  
+  // validate title
+  const title = formData.get("title") as string
+  if (title == "") {
+    return { error: "Title can not be empty."}
+  }
+  
+  // validate body
+  const body = formData.get("body") as string
+  if (body.length < 30) {
+    return { error: "Body should be more then 30 words." }
+  }
 
   const label = formData.get("label") as string
   const post: CreatePostType = {
     owner: process.env.GITHUB_USER_NAME as string,
     repo: process.env.GITHUB_REPO_NAME as string,
-    title: formData.get("title") as string,
-    body: formData.get("body") as string,
+    title: title,
+    body: body,
     labels: label.length > 0 ? [label] : undefined
   }
-
+  
+  const session = (await auth()) as SessionWithToken
   const octokit = new Octokit({
     auth: session.accessToken
   })
 
-  await octokit.rest.issues.create(post)
+  await octokit.rest.issues.create(post).catch(err => {
+    return { error: err }
+  })
+
+  // redirect to home when create finished
   redirect("/")
 }
