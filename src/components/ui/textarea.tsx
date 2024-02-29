32f28dcac6992ebbbf98caa6e5b1d7902cf12d7e
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-02-29 13:30:07 +0800

    fix(shadcn): change input, textarea bg in dark mode

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -10,7 +10,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          "flex min-h-[80px] w-full rounded-md border border-slate-200 bg-white px-3 py-2 text-sm placeholder:text-slate-500 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400",
+          "flex w-full min-h-[80px] rounded-md border border-slate-200 bg-white px-3 py-2 text-sm placeholder:text-gray focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray3 dark:bg-gray5 dark:placeholder:text-gray",
           className
         )}
         ref={ref}
