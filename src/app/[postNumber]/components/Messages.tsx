d0cd1447ac9a2633fb5b3a4c072e635c6873f65f
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-03-01 11:27:24 +0800

    fix(Messages): move messages fetching func to parent node and add key setting

--- src/app/[postNumber]/components/Messages.tsx
+++ src/app/[postNumber]/components/Messages.tsx
@@ -1,24 +1,23 @@
 // actions
-import { getMessages } from "@/actions/get-messages";
+import { getMessages } from "@/actions/messages.get";
 // components
 import Message from "@/components/Message";
 import { Separator } from "@/components/ui/separator";
 // utils
 import { cn } from "@/lib/utils";
 
-export const Messages = async ({ postNumber }: { postNumber: number }) => {
-  const messages = await getMessages(postNumber);
+export const Messages = ({ messages }: { messages: Message[] }) => {
   return (
     <div>
       <div className={cn("text-3xl font-semibold tracking-tight pb-4")}>
-        Comments
+        Comments ({messages.length})
       </div>
       {messages.map((message) => {
         return (
-          <>
+          <div key={message.id}>
             <Message message={message} />
             <Separator />
-          </>
+          </div>
         );
       })}
     </div>
