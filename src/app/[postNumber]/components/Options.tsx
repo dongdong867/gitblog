20c179c0d041fcf62abcb422af4526ecfa7f4d9c
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-03-01 11:27:45 +0800

    feat(Options): create post edit and delete options

---
+++ src/app/[postNumber]/components/Options.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { HiDotsHorizontal } from "react-icons/hi";
+
+export const Options = ({ postNumber }: { postNumber: number }) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          size={"icon"}
+          variant={"ghost"}
+          className={cn("size-max p-2 rounded-full")}
+        >
+          <HiDotsHorizontal size={20} className={cn("dark:hover:text-white")} />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem asChild>
+          <Link href={`${postNumber}/edit`}>Edit</Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          className={cn(
+            "text-red focus:text-white focus:bg-red",
+            "dark:focus:text-white dark:focus:bg-red"
+          )}
+        >
+          Delete
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
