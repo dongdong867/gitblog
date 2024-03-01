fdc9e0927448125b21213cab8723720b48a13299
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-03-01 11:26:38 +0800

    feat(BackLink): move text style to parent nodes

--- src/app/[postNumber]/components/BackLink.tsx
+++ src/app/[postNumber]/components/BackLink.tsx
@@ -7,7 +7,7 @@ import { cn } from "@/lib/utils";
 
 export const BackLink = ({ createAt }: { createAt: string }) => {
   return (
-    <Link href={"/"} className={cn("text-sm text-gray no-underline")}>
+    <Link href={"/"}>
       <div className={cn("flex place-items-center space-x-2 py-2")}>
         <FaArrowLeftLong size={14} />
         <div>{new Date(createAt).toLocaleDateString()}</div>
