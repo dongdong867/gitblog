dd69a56535e0710b423763f48a70353237ac2a99
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-03-03 03:14:53 +0800

    feat(robots): create robots.ts file for crawling

---
+++ src/app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from "next";
+
+const robots = (): MetadataRoute.Robots => {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: ["/api/auth/", "/create", "/*/edit"],
+    },
+  };
+};
